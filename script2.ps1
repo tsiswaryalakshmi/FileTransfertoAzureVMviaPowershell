ea34b128eda4dfd2ea7fa811111a45c7c74a8af4
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-11-02 16:21:37 +0530

    Update script2.ps1

--- script2.ps1
+++ script2.ps1
@@ -3,5 +3,5 @@ $password = ConvertTo-SecureString "Welcome.1234" -AsPlainText -Force
 $Cred = New-Object System.Management.Automation.PSCredential ("powershell", $password)
 $s =  New-PSSession -ComputerName "powershell1.eastus.cloudapp.azure.com" -Credential $cred -Authentication CredSSP
 Enable-WSManCredSSP -Role client -DelegateComputer powershell1.eastus.cloudapp.azure.com -Force
-Copy-Item "sampleRepo-main.zip" C:\Users\powershell\Desktop -ToSession $s
+Copy-Item "npp.8.4.2.Installer.x64.zip" C:\Users\powershell\Desktop -ToSession $s
 #Expand-Archive -Path C:\Users\powershell\Desktop\sampleRepo-main.zip -DestinationPath C:\Users\powershell\Desktop\
